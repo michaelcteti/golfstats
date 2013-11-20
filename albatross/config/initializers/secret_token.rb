446e55935f1cdd2c08214337245b7e385f7dc5cf
Michael Teti <michael.teti@arcadiasolutions.com>
2013-11-20 08:28:53 -0500

    Add initial Rails application

---
+++ albatross/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albatross::Application.config.secret_token = 'c20d75f8884eaccc5c534c2bb6b66a313715eedf17774ea2dd552d062c2d4c0b10db4b1cd00458023b25e54d98d26b4b90de0cb76cf58e03370220cbb07a3b34'
